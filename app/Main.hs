d5e8a9b420a7429212bada41ec57fc4e83218e95
stev <stefan.jaax@gmail.com>
2020-11-07 22:35:17 +0100

    Crawl repository URLs

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,12 @@ module Main where
 
 import Github.Crawler
 import qualified Data.Text.IO as T
+import Data.Containers.ListUtils (nubOrd)
 
 main :: IO ()
 main = do
     timelineContent <- crawlTimeline "nh2"
-    print timelineContent
+    case timelineContent of
+    	Right content -> print $ nubOrd content
+    	Left _ -> print timelineContent
 
--- src/Github/Crawler.hs
+++ src/Github/Crawler.hs
@@ -2,33 +2,21 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 
-module Github.Crawler ( TimelineContent(..)
-                      , crawlTimeline
+module Github.Crawler ( crawlTimeline
                       ) where
 
 import qualified Data.Text as T
 import           Data.Text (Text)
 import           Text.HTML.Scalpel
 import           Control.Applicative ((<|>))
+import           Control.Concurrent (threadDelay)
 import           Control.Monad (guard)
 import           Debug.Trace (trace)
 import           Network.HTTP.Client.TLS (newTlsManager)
 type Username = String
 
-data TimelinePullRequestContent =
-  TimelinePullRequestContent
-    { repository :: Text
-    } deriving (Eq, Ord, Show)
+type RepoURL = T.Text
 
-data TimelineIssueContent =
-  TimelineIssueContent
-    { repository :: Text
-    } deriving (Eq, Ord, Show)
-
-data TimelineContent
-  = TimelineContentIssue TimelineIssueContent
-  | TimelineContentPullRequest TimelinePullRequestContent
-  deriving (Eq, Ord, Show)
 
 getConfig :: IO (Config Text)
 getConfig = do
@@ -37,20 +25,21 @@ getConfig = do
                   , manager = Just m
                   }
 
-crawlTimeline :: Username -> IO (Either Text [TimelineContent])
+crawlTimeline :: Username -> IO (Either Text [RepoURL])
 crawlTimeline userName = do
   cfg <- getConfig
   crawlTimelineURL "https://github.com/nh2?tab=overview&from=2019-07-01&to=2019-07-31" cfg
 --crawlTimelineURL ("https://github.com/" ++ userName)
 
 
-crawlTimelineURL :: URL -> Config Text -> IO (Either Text [TimelineContent])
+crawlTimelineURL :: URL -> Config Text -> IO (Either Text [RepoURL])
 crawlTimelineURL url cfg = do
   next <- scrapeURLWithConfig cfg url scrapeTimeLineContent
-  print next
   case next of
     Just (timelineContent, Nothing) -> return $ Right timelineContent
     Just (timelineContent, Just nextURL) -> do
+      print (timelineContent, nextURL)
+      threadDelay 500000 -- 1 second because of rate limit
       eOlderContents <- crawlTimelineURL nextURL cfg
       return $ case eOlderContents of
         Left err -> Left err
@@ -58,42 +47,15 @@ crawlTimelineURL url cfg = do
     Nothing -> return $ Left $ T.pack $ "Could not scrape at URL " ++ url
 
 
-scrapeRepository :: Scraper Text Text
-scrapeRepository = attr "data-hovercard-url" $
-    "span" @:
-    [ hasClass "css-truncate"
+scrapeRepositoryURLs :: Scraper Text [RepoURL]
+scrapeRepositoryURLs = attrs "href" $
+    "a" @:
+    [ hasClass "link-gray-dark"
     , "data-hovercard-type" @= "repository"
     ]
 
 
-scrapeTimelinePullRequestContent :: Scraper Text TimelinePullRequestContent
-scrapeTimelinePullRequestContent = do
-  repository <- scrapeRepository
-  return $ TimelinePullRequestContent
-    { repository = repository
-    }
-
-
-scrapeTimelineIssueContent :: Scraper Text TimelineIssueContent
-scrapeTimelineIssueContent = do
-  repository <- scrapeRepository
-  return TimelineIssueContent
-    { repository = repository
-    }
-
-
-scrapePullRequestRollupWrapper :: Scraper Text [TimelineContent]
-scrapePullRequestRollupWrapper = do
-  matches $ "svg" @: [hasClass "octicon-git-pull-request"]
-  chroots ("div" @: [hasClass "profile-rollup-summarized"]) $
-    TimelineContentPullRequest <$> scrapeTimelinePullRequestContent
-
-
-scrapeTimelineWrapper :: Scraper Text [TimelineContent]
-scrapeTimelineWrapper = scrapePullRequestRollupWrapper
-
-
-scrapeTimeLineContent :: Scraper Text ([TimelineContent], Maybe URL)
+scrapeTimeLineContent :: Scraper Text ([RepoURL], Maybe URL)
 scrapeTimeLineContent = do
   let doesButtonExist = do
         t <- text $ "button" @: [hasClass "ajax-pagination-btn"]
@@ -105,7 +67,7 @@ scrapeTimeLineContent = do
         else
           return Nothing
   url <- fmap ("https://github.com" ++) <$> getAction
-  contents <- concat <$> chroots ("div" @: [hasClass "profile-rollup-wrapper"]) scrapeTimelineWrapper
+  contents <- concat <$> chroots ("div" @: [hasClass "TimelineItem"]) scrapeRepositoryURLs
   return (contents, url)
 
 
